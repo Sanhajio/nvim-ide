062eb92f919c300709faa96157750437c9aa38a6
Sanhaji Omar <omar.sanhaji@easybourse.com>
2024-03-13 20:55:18 +0100

    Adding rust formatter

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,63 +1,74 @@
 return {
-    "mhartington/formatter.nvim",
-    version = "*",
-    config = function()
-        local util = require "formatter.util"
-        local lsp_util = require("lspconfig/util")
+  'mhartington/formatter.nvim',
+  version = '*',
+  config = function()
+    local util = require 'formatter.util'
+    local lsp_util = require 'lspconfig/util'
 
-        -- au BufWritePre * try | undojoin | :FormatWrite | catch /E790/ | :FormatWrite | endtry
+    -- au BufWritePre * try | undojoin | :FormatWrite | catch /E790/ | :FormatWrite | endtry
 
-        vim.api.nvim_exec(
-            [[
+    vim.api.nvim_exec(
+      [[
               augroup FormatOnSave
                 autocmd!
                 autocmd BufWritePost * FormatWrite
               augroup end
             ]],
-            true
-        )
+      true
+    )
 
-        require("formatter").setup {
-            logging = true,
-            log_level = vim.log.levels.WARN,
+    require('formatter').setup {
+      logging = true,
+      log_level = vim.log.levels.WARN,
 
-            filetype = {
-                cs = {
-                    function()
-                        return {
-                            exe = "dotnet",
-                            args = {
-                                "csharpier"
-                            },
-                            cwd = vim.fn.expand("%:p:h"),
-                            stdin = true,
-                            env = {
-                                "DOTNET_CLI_TELEMETRY_OPTOUT=true",
-                                "TERM=xterm-256color"
-                            }
-                        }
-                    end
-                },
-                lua = {
-                    require("formatter.filetypes.lua").stylua,
-                },
-                python = {
-                    require("formatter.filetypes.python").black,
-                },
-                json = {
-                    require("formatter.filetypes.json").jsbeautify,
-                },
-                typescriptreact = {
-                    require("formatter.filetypes.typescriptreact").prettier,
-                },
-                typescript = {
-                    require("formatter.filetypes.typescript").prettier,
-                },
-                go = {
-                    require("formatter.filetypes.go").gofmt,
-                    require("formatter.filetypes.go").goimports,
-                }
+      filetype = {
+        rust = {
+          function()
+            return {
+              exe = 'rustfmt',
+              args = {
+                '--edition 2021',
+              },
+              stdin = true,
             }
-        }
-    end
+          end,
+        },
+        cs = {
+          function()
+            return {
+              exe = 'dotnet',
+              args = {
+                'csharpier',
+              },
+              cwd = vim.fn.expand '%:p:h',
+              stdin = true,
+              env = {
+                'DOTNET_CLI_TELEMETRY_OPTOUT=true',
+                'TERM=xterm-256color',
+              },
+            }
+          end,
+        },
+        lua = {
+          require('formatter.filetypes.lua').stylua,
+        },
+        python = {
+          require('formatter.filetypes.python').black,
+        },
+        json = {
+          require('formatter.filetypes.json').jsbeautify,
+        },
+        typescriptreact = {
+          require('formatter.filetypes.typescriptreact').prettier,
+        },
+        typescript = {
+          require('formatter.filetypes.typescript').prettier,
+        },
+        go = {
+          require('formatter.filetypes.go').gofmt,
+          require('formatter.filetypes.go').goimports,
+        },
+      },
+    }
+  end,
 }
